bf4694ba2958b3783767d6a2d74f90823757fbe6
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-09-16 11:56:20 +0100

    :construction: Adding Weapon methods and MathUtility

---
+++ IntroToCSharp/IntroToCSharp/GD3A/Utilities/MathUtility.cs
@@ -0,0 +1,25 @@
+﻿namespace IntroToCSharp.GD3A.Utilities
+{
+    public static class MathUtility
+    {
+        public static readonly double FLOAT_TOLERANCE = 10E-6;
+
+        /// <summary>
+        /// Compares two double within a given epsilon.
+        /// </summary>
+        /// <param name="a">Double a</param>
+        /// <param name="b">Double b</param>
+        /// <param name="epsilon">Tolerance value</param>
+        /// <returns>True if less than epsilon</returns>
+        public static bool AreEqualWithin(double a, double b,
+            double epsilon)
+        {
+            return Math.Abs(a - b) < epsilon;
+        }
+
+        public static bool AreEqualWithin(double a, double b)
+        {
+            return Math.Abs(a - b) < FLOAT_TOLERANCE;
+        }
+    }
+}
--- IntroToCSharp/IntroToCSharp/GD3A/Weapon.cs
+++ IntroToCSharp/IntroToCSharp/GD3A/Weapon.cs
@@ -1,4 +1,6 @@
-﻿namespace IntroToCSharp.GD3A
+﻿using IntroToCSharp.GD3A.Utilities;
+
+namespace IntroToCSharp.GD3A
 {
     internal class Weapon
     {
@@ -33,6 +35,35 @@
         #endregion Constructors
 
         //TODO - NMCG - Add ToString, Equals, Properties
+        public override string ToString()
+        {
+            //string interpolation example
+            return $"Weapon: {name},{damage},{ammoCount},{IsEquipped}";
+        }
+
+        public bool Equals(Weapon other)
+        {
+            return MathUtility.AreEqualWithin(this.damage,
+                other.damage, MathUtility.FLOAT_TOLERANCE);
+
+            //MathUtility mathUtility = new MathUtility();
+            //return mathUtility.AreEqualWithin(this.damage, other.damage,
+            //    0.0001f);
+            //return this.damage == other.damage;
+        }
+
+        /// <summary>
+        /// Compares two floats within a given epsilon.
+        /// </summary>
+        /// <param name="a">Float a</param>
+        /// <param name="b">Float b</param>
+        /// <param name="epsilon">Tolerance value</param>
+        /// <returns>True if less than epsilon</returns>
+        //public bool AreEqualWithin(float a, float b,
+        //    float epsilon)
+        //{
+        //    return Math.Abs(a - b) < epsilon;
+        //}
 
         //RFR - NMCG - Make this run within 60-80 FPS
     }
