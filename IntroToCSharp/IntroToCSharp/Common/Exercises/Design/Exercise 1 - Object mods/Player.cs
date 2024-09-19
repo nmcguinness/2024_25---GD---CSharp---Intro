87c3125a3e92f845a7230ec5fae84cd2b644019f
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-09-19 09:41:15 +0100

    Update Player.cs

--- IntroToCSharp/IntroToCSharp/Common/Exercises/Design/Exercise 1 - Object mods/Player.cs
+++ IntroToCSharp/IntroToCSharp/Common/Exercises/Design/Exercise 1 - Object mods/Player.cs
@@ -6,10 +6,31 @@
     public class Player : GameObject
     {
         private static readonly float START_Y_POSITION = 10;
+
+        #region Fields
+
         public int health;
 
+        #endregion Fields
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value >= PLAYER_HEALTH_MIN && value <= PLAYER_HEALTH_MAX
+                    ? value
+                    : PLAYER_HEALTH_MIN;
+            }
+        }
+
+        #region Constructors
+
         public Player(string iD)
-         : base(iD, true, 0, START_Y_POSITION, 0)
+      : base(iD, true, 0, START_Y_POSITION, 0)
         {
             this.health = 100;
         }
@@ -28,9 +49,15 @@
             this.health = health;
         }
 
+        #endregion Constructors
+
+        #region Methods - Housekeeping
+
         public override string ToString()
         {
-            return $"Player: {id} at ({x}, {y}, {z}) with {health} health";
+            return $"Player: {ID} at ({X}, {Y}, {Z}) with {health} health";
         }
+
+        #endregion Methods - Housekeeping
     }
 }
